2d587d9612f3296f8a6b027a8629af2b11fd00fa
Reed Allman <rdallman10@gmail.com>
2013-07-08 20:40:25 -0500

    10 at a time

--- pc.c
+++ pc.c
@@ -5,12 +5,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <semaphore.h>
 
-#define THREADS 1
 #define ROW 10
 #define COL 63
 
-pthread_barrier_t barr;
+pthread_mutex_t mutex;
+int threads;
 
 void * do_crunch();
 void * do_gobble();
@@ -20,38 +21,33 @@ void * do_producer() {
   char *line = NULL;
   size_t size;
   int i = 0;
-  if(pthread_barrier_init(&barr, NULL, THREADS)) {
-    printf("Could not create a barrier\n");
-    return -1;
-  }
+  threads = 0;
+
   while (getline(&line, &size, stdin) > -1) {
-    printf("%s", line);
     pthread_t crunch;
 
     if(pthread_create(&crunch, NULL, &do_crunch, line)) {
       printf("Couldn't create thread\n");
     }
 
-    if (pthread_join(crunch, NULL)) {
-      printf("Could not join thread\n");
-    }
     i++;
-    if ( i % 10 == 0 ) {
-      int rc = pthread_barrier_wait(&barr);
-      if (rc != 0 && rc != PTHREAD_BARRIER_SERIAL_THREAD) {
-        printf("Could not wait on barrier\n");
-        exit(-1);
+    threads++;
+    //printf("%d", threads);
+
+    if ( threads == 10 ) {
+      if(pthread_join(crunch, NULL))
+      {
+        printf("Could not join thread\n");
       }
     }
 
   }
-  printf("Total lines: %d", i);
+  printf("\n\nTotal lines: %d", i);
 }
 
 void * do_crunch(char *line) {
   //do some stuff
   char *s;
-  //printf("%s", line);
 
   s = strchr(line, ' ');
   while (s != NULL) {
@@ -77,7 +73,6 @@ void * do_gobble(char *line) {
     line[i] = toupper(line[i]);
     i++;
   }
-  //printf("%s", line);
   pthread_t consumer;
   if(pthread_create(&consumer, NULL, &do_consumer, line)) {
     printf("Couldn't create thread\n");
@@ -90,11 +85,8 @@ void * do_gobble(char *line) {
 }
 
 void * do_consumer(char *line) {
-  FILE *file;
-  file = fopen("output.txt", "a+");
-  fprintf(file, "%s", line);
-  fclose(file);
-
+  printf("%s", line);
+  threads--;
 }
 
 int main(int argc, char **argv) {
@@ -120,7 +112,6 @@ int main(int argc, char **argv) {
   }
   printf(b[0]);
   */
-  printf("here");
 
   pthread_t producer;
 
